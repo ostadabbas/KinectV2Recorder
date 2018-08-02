2b474e6e15b7632b0af24d6e2564590761b42aec
Sarah Ostadabbas <ostadabbas@gmail.com>
2018-08-02 10:40:06 -0400

    updated the readFrames to output point cloud

--- readFrames.py
+++ readFrames.py
@@ -2,15 +2,16 @@
 
 from __future__ import division
 
-import numpy as np
 import gzip
+import scipy.misc
 
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 import imageio
 
-def loadFrames(fname):
+def loadFrames(fname, startFrame=0, nFrames=200):
     w,h,headerSize = 512,424,4
 
     time_s = None
@@ -36,11 +37,14 @@ def loadFrames(fname):
         frames = data.reshape((-1, fLen))
         data = []
 
+        frames = frames[startFrame:nFrames,:]
+
         # convert frames
         frames = np.cumsum(frames,axis=0) # reverse the delta compression
         frames = frames.reshape((-1,h,w)).astype(float) # convert to floating point
 
     vfname = os.path.join(fname, 'rgb.avi')
+    vid = None
     if os.path.exists(vfname):
         vid = imageio.get_reader(vfname, 'ffmpeg')
 
@@ -50,10 +54,25 @@ def loadFrames(fname):
 
 # Demonstration: feel free to delete this
 # fname = 'Kinect_FileName_2017-2-19-03-15-52_depth.bin.gz'
-fname = 'Kinect_FileName_2017-4-9-06-48-02'
-time_s, frames, vid = loadFrames('Kinect Data/' + fname)
+#fname = 'Kinect_FileName_2017-4-9-06-48-02'
+fname2 = 'C:\Users\AC-Lab\Documents\RecordedPC\Kinect_30sec_2018-07-26-04-24-24'
+outdir = 'C:\Users\AC-Lab\Documents\RecordedPC\Kinect_30sec_2018-07-26-04-24-24'
+#time_s, frames, vid = loadFrames('Kinect Data/' + fname)
 
+startFrame=0
+nFrames=55
 fn = 10
+saveFrames=[1,20,40,50]
+
+
+time_s, frames, vid = loadFrames(fname2, startFrame, nFrames)
+
+for n in saveFrames:
+    print(n)
+    bname = os.path.join(outdir, 'out{0:02d}'.format(n))
+    np.savetxt('{}.csv'.format(bname), frames[n], delimiter=',')
+    scipy.misc.imsave('{}.jpg'.format(bname), vid.get_data(n))
+
 plt.figure('frame {}'.format(fn))
 plt.imshow(frames[fn]) # show frame 10
 
